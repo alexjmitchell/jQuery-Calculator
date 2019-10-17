fe2e7aa719ff21de5291d0c8b50c1653086dc0d1
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-10-17 09:49:17 -0700

    made adjustments to calc

--- Adventure-Mode/main.js
+++ Adventure-Mode/main.js
@@ -4,31 +4,46 @@ $(document).ready(function() {
   let oper = ""
   $(".container").on("click", "button", function(e) {
     var val = $(this).html()
+    function result() {
+      if (val === "=") {
+        let answer = null
+        if (oper === "+") {
+          answer = Number(num1) + Number(num2)
+        } else if (oper === "-") {
+          answer = Number(num1) - Number(num2)
+        } else if (oper === "*") {
+          answer = Number(num1) * Number(num2)
+        } else if (oper === "/") {
+          answer = Number(num1) / Number(num2)
+        }
 
-    if (val === "=") {
-      if (oper === "+") {
-        $("#results").html(Number(num1) + Number(num2))
-      } else if (oper === "-") {
-        $("#results").html(Number(num1) - Number(num2))
-      } else if (oper === "*") {
-        $("#results").html(Number(num1) * Number(num2))
-      } else if (oper === "/") {
-        $("#results").html(Number(num1) / Number(num2))
-      }
-    } else if (val === "+" || val === "-" || val === "*" || val === "/") {
-      oper = val
-    } else if (val === "C") {
-      num1 = ""
-      num2 = ""
-      oper = ""
-      $("#results").html("")
-    } else {
-      if (oper === "") {
-        num1 += val
+        $("#results").html(answer)
+
+        num1 = answer
+      } else if (val === "+" || val === "-" || val === "*" || val === "/") {
+        oper = val
+        num2 = ""
+      } else if (val === "C") {
+        num1 = ""
+        num2 = ""
+        oper = ""
+        $("#results").html("")
       } else {
-        num2 += val
+        if (oper === "") {
+          if ($("#decimal").html() === ".") {
+            num1 + val + num1
+          }
+          num1 += val
+          $("#results").html(Number(num1))
+        } else {
+          if ($("#decimal").html() === ".") {
+            num2 + val + num2
+          }
+          num2 += val
+          $("#results").html(Number(num2))
+        }
       }
     }
-    console.log(num1, oper, num2)
+    result()
   })
 })
