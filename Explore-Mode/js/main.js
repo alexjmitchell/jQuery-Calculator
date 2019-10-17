822a5a7df2fe607d9c4cbd5bf3c379e28b3ec411
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-10-16 17:15:42 -0700

    not making much progress, probably starting over.

--- Explore-Mode/js/main.js
+++ Explore-Mode/js/main.js
@@ -1,15 +1,71 @@
+// $(document).ready(function() {
+//   $("#calculator").on("submit", function(e) {
+//     e.preventDefault()
+
+//     var num_one = Number($("#num_one").val())
+//     console.log(num_one)
+//     var num_two = Number($("#num_two").val())
+//     console.log(num_two)
+
+//     let add = $("#add").html()
+//     let sub = $("#subtraction").html()
+//     let div = $("#division").html()
+//     let mult = $("#multiplication").html()
+//     let oper = add || sub || div || mult
+
+//     const subtraction = () => {
+//       $("#results").html(`${num_one} - ${num_two} = ${num_one - num_two}`)
+//     }
+
+//     const addition = () => {
+//       $("#results").html(`${num_one} + ${num_two} = ${num_one + num_two}`)
+//     }
+
+//     const multiplication = () => {
+//       $("#results").html(`${num_one} * ${num_two} = ${num_one * num_two}`)
+//     }
+
+//     const division = () => {
+//       $("#results").html(`${num_one} / ${num_two} = ${num_one / num_two}`)
+//     }
+
+//     const calculate = () => {
+//       if (oper === sub) {
+//         subtraction()
+//       } else if (oper === add) {
+//         addition()
+//       } else if (oper === mult) {
+//         multiplication()
+//       } else if (oper === div) {
+//         division()
+//       } else {
+//         throw new Error("Something is not working!!!!")
+//       }
+//     }
+
+//     calculate()
+//     console.log(oper)
+//   })
+// })
+
 $(document).ready(function() {
   $("#calculator").on("submit", function(e) {
     e.preventDefault()
 
     var num_one = Number($("#num_one").val())
-
+    console.log(num_one)
     var num_two = Number($("#num_two").val())
+    console.log(num_two)
+  })
 
-    var oper = $(".operator").html()
+  $("button").on("submit", function(e) {
+    e.preventDefault()
 
-    // console.log("number one:" + num_one)
-    // console.log("number two:" + num_two)
-    console.log(num_one + oper + num_two)
+    var num_one = Number($("#num_one").val())
+    console.log(num_one)
+    var num_two = Number($("#num_two").val())
+    console.log(num_two)
+
+    console.log($(".operator"))
   })
 })
